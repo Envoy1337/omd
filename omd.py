ee9dd1e312b0702ac452aae5cce5f7b7ead047da
Envoy1337 <72042901+Envoy1337@users.noreply.github.com>
2023-11-22 20:27:18 +0300

    Update omd.py

--- omd.py
+++ omd.py
@@ -1,39 +1,24 @@
 import click
-from random import randint
-from typing import Dict
+from Pizza import Pizza
+from decorator import log
 
 
-class Pizza:
-    def __init__(self, name, toppings):
-        self.name = name
-        self.toppings = toppings
-
-    def dict(self) -> Dict[str, list]:
-        return {self.name: self.toppings}
-
-    def __eq__(self, other) -> bool:
-        return self.name == other.name and self.toppings == other.toppings
+@log('Пицца приготовится за')
+def bake(pizza: Pizza) -> None:
+    """ Пицца готовится """
+    pass
 
 
-class PizzaOrder:
+@log('Доставим пиццу за')
+def deliver(pizza: Pizza) -> None:
+    """ Пицца доставляется"""
+    pass
 
-    @staticmethod
-    def prepare_pizza(pizza, delivery=False):
-        """ Приготовление пиццы и вывод времени приготовления и доставки"""
-        if delivery:
-            s = randint(1, 10)
-            print(f"Prepared in {s}s")
-            s = randint(1, 10)
-            print(f"Delivered in {s}s")
-        else:
-            s = randint(1, 10)
-            print(f"Prepared in {s}s")
 
-    @staticmethod
-    def display_menu(pizzas):
-        for pizza in pizzas:
-            toppings = ', '.join(pizzas[pizza])
-            print(f"- {pizza}: {toppings}")
+def display_menu(pizzas: dict):
+    for pizza in pizzas:
+        toppings = ', '.join(pizzas[pizza])
+        print(f"- {pizza}: {toppings}")
 
 
 @click.group()
@@ -43,9 +28,13 @@ def cli():
 
 @cli.command()
 @click.option('--delivery', is_flag=True, help='Deliver the pizza')
+@click.option('--size', default='L', help='Pizza size (L/XL)')
 @click.argument('pizza', nargs=1)
-def order(pizza, delivery):
-    """ Выполнение команды order, приготовление пиццы и ее доставка(если указан флаг)"""
+def order(pizza: Pizza, delivery: bool):
+    """
+    Выполнение команды order,
+    приготовление пиццы и ее доставка(если указан флаг)
+    """
     menu = {
         "Margherita": ["tomato sauce", "mozzarella", "tomatoes"],
         "Pepperoni": ["tomato sauce", "mozzarella", "pepperoni"],
@@ -53,21 +42,25 @@ def order(pizza, delivery):
     }
     if pizza in menu:
         selected_pizza = Pizza(pizza, menu[pizza])
-        PizzaOrder.prepare_pizza(selected_pizza, delivery)
+        bake(selected_pizza)
+        if delivery:
+            deliver(selected_pizza)
+
     else:
         print("This pizza is not in menu")
 
 
 @cli.command()
 def menu():
-    """Выполнение команды order"""
+    """Выполнение команды menu"""
     menu = {
         "Margherita ": ["tomato sauce", "mozzarella", "tomatoes"],
         "Pepperoni ": ["tomato sauce", "mozzarella", "pepperoni"],
         "Hawaiian ": ["tomato sauce", "mozzarella", "chicken", "pineapples"]
     }
-    PizzaOrder.display_menu(menu)
+    display_menu(menu)
 
 
 if __name__ == '__main__':
     cli()
+
