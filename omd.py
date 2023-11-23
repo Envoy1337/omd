242fb1e6e444cf2d2417ae1ff4db3afcc72ce74e
Envoy1337 <72042901+Envoy1337@users.noreply.github.com>
2023-11-23 15:20:07 +0300

    Update omd.py

--- omd.py
+++ omd.py
@@ -1,26 +1,36 @@
-class my_CountVectorizer:
-    mp_words = []
+class CountVectorizer:
+    def __init__(self):
+        self.voc = {}
+        self.mp_words  = []
 
-    def my_fit_transform(self, text=None):
-        res = []
-        my_CountVectorizer.mp_words = []
-        my_CountVectorizer.S = []
-        for line in text:
-            my_CountVectorizer.S.append(line)
-            cur = line.lower()
-            cur_s = cur.split()
-            for i in range(len(cur_s)):
-                if cur_s[i] not in my_CountVectorizer.mp_words:
-                    my_CountVectorizer.mp_words.append(cur_s[i])
-        for line in my_CountVectorizer.S:
-            cur = line.lower()
-            cur_s = cur.split()
-            cur_res = [0] * len(my_CountVectorizer.mp_words)
-            for j in range(len(my_CountVectorizer.mp_words)):
-                cur_res[j] = cur_s.count(my_CountVectorizer.mp_words[j])
-            res.append(cur_res)
-        return res
+    def fit_transform(self, corpus):
+        """Подсчет частоты каждого слова в документах"""
+        for corp in corpus:
+            words = corp.split()
+            for cur in words:
+                if cur not in self.voc:
+                    self.voc[cur] = len(self.voc)
+        count_matrix = []
+        for corp in corpus:
+            count = [0] * len(self.voc)
+            words = corp.split()
+            for word in words:
+                count[self.voc[word]] += 1
+            count_matrix.append(count)
 
-    def my_get_feature_names(self):
-        return my_CountVectorizer.mp_words
+        return count_matrix
 
+    def get_feature_names(self):
+        """Получаем слова в порядке их появления в словаре"""
+        self.mp_words = [word for word, _ in self.voc.items()]
+        return self.mp_words
+
+
+if __name__ == '__main__':
+    documents = ["""Crock Pot Pasta Never boil pasta again""",
+                 """Pasta Pomodoro
+                    Fresh ingredients Parmesan to taste"""]
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(documents)
+    print(count_matrix)
+    print(vectorizer.get_feature_names())
